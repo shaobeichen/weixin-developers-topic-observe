18a68f2d9b2f7ebea6a8e3f78f21888408ac36d2
LeachZhou <leachzhou@163.com>
2021-01-27 13:46:36 +0800

    feat: update action

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     //邮件配置
     email: {
         service: 'QQ',
-        user: '你的邮箱',
-        pass: '你的密码',
+        user: process.env.U_MAIL,
+        pass: process.env.U_PASSWORD,
     }
 }
